ac42dcee4021a4017d559736eef0284cec5d4f7d
Dee Kay <dk@email.com>
2023-01-15 23:58:41 +0000

    project start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+
+let firstCard = 0;
+let secondCard = 0;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+function check(num) {
+  if (num === 21) {
+    message = "win 🥳";
+    hasBlackJack = true;
+  }
+  else if (num < 21) {
+    message = "continue 🙂";
+  }
+  else {
+    message = "out of the game 😭";
+    isAlive = false;
+  }
+}
+
+console.log(message);
+
+function reset() {
+  firstCard = 0;
+  secondCard = 0;
+  hasBlackJack = false;
+  isAlive = true;
+  message = "";
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BlackJack</h1>
+        <div>
+          <p id="message-el">Want to play a round?</p>
+          <p>Cards:</p>
+          <p>Sum:</p>
+        </div>
       </header>
     </div>
   );
