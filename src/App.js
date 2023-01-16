720269bfc6353eeeab6bc61aa4a679455174ef5b
Dee Kay <dk@email.com>
2023-01-16 22:40:53 +0000

    getRandomCard function

--- src/App.js
+++ src/App.js
@@ -53,7 +53,14 @@ function newCard(){
 }
 
 function getRandomCard(){
-  
+  let randomNumber = Math.floor(Math.random()*13) + 1;
+  if (randomNumber >  10){
+    return 10;
+  } else if (randomNumber === 1){
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function App() {
